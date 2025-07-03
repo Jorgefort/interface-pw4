f8f5fcc31e54cdce6da26a2584578d31087960f9
jorge <102530@glr.nl>
2025-07-03 14:17:46 +0000

    finished

--- README.md
+++
@@ -1,1 +0,0 @@
-# interface-pw4
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./js/**/*.js",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mono': ['JetBrains Mono', 'monospace'],
+      },
+      colors: {
+        'luxury-black': '#000000',
+        'luxury-gray': '#1a1a1a',
+        'luxury-light': '#2a2a2a',
+      }
+    },
+  },
+  plugins: [],
+}
